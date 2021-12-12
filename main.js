63a274f193eb7e34d67c5393d9bfef72ea462da2
Thanuk <86400849+Thanuk@users.noreply.github.com>
2021-12-12 14:52:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var canvas = new fabric.Canvas('myCanvas');
 var x = document.getElementById("myAudio");
 
 function new_image(){
-    fabric.Image.fromUrl("birthday.jpg" , function(Img){ 
+    fabric.Image.fromURL("BirthdayImage.jpg" , function(Img){ 
         block_image_object = Img;
         block_image_object.scaleToWidth(700);
         block_image_object.scaleToHeight(510);
